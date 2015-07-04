b5951278cdebea9ae68d8f59c013f80081a0126d
cacao <kaorin10815@gmail.com>
2015-07-04 16:56:39 +0900

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "UserPages" do
+  subject {page}
+
+  describe "signup page" do
+       before {visit 'users_new_path'}
+
+    it { should have_content 'Sign up' }
+    it { should have_title 'full_titile("Sign up")' }
+  end
+  end
+end
